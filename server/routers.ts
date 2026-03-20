a543936d8b2a5ca685e52544793ff13245999b82
LukasClay <98345038+LukasClay@users.noreply.github.com>
2026-03-20 00:58:33 -0400

    revert: auth.me endpoint to publicProcedure

--- server/routers.ts
+++ server/routers.ts
@@ -15,7 +15,7 @@ export const appRouter = router({
   system: systemRouter,
 
   auth: router({
-    me: protectedProcedure.query(opts => opts.ctx.user),
+    me: publicProcedure.query(opts => opts.ctx.user),
     logout: publicProcedure.mutation(({ ctx }) => {
       const cookieOptions = getSessionCookieOptions(ctx.req);
       ctx.res.clearCookie(COOKIE_NAME, { ...cookieOptions, maxAge: -1 });
